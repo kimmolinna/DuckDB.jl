8526fead875a795df22072acbc7dfc264d5f74fa
Lars Dammann <lars.dammann@desy.de>
2022-02-10 12:31:23 +0100

    Implemented appender API tests for some data types

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,5 @@
 using Test
-using DataFrames, DBInterface, CSV
+using DataFrames, DBInterface, CSV, Dates
 using DuckDB
 #include("../src/DuckDB.jl")
 
@@ -7,6 +7,62 @@ using DuckDB
     db = DuckDB.open(":memory:")
     con = DuckDB.connect(db)
     @test isa(con, Base.RefValue{Ptr{Nothing}})
+    DuckDB.disconnect(con)
+    DuckDB.close(db)
+end
+
+@testset "Appender API" begin
+    # Open the database
+    db = DuckDB.open(":memory:")
+    con = DuckDB.connect(db)
+
+    # Create the table the data is appended to
+    DuckDB.execute(con, "CREATE TABLE dtypes(bol BOOLEAN, tint TINYINT, sint SMALLINT, int INTEGER, bint BIGINT, utint UTINYINT, usint USMALLINT, uint UINTEGER, ubint UBIGINT, float FLOAT, double DOUBLE, date DATE, time TIME, vchar VARCHAR)")
+
+    # Create the appender
+    appender = DuckDB.appender_create(con, "main", "dtypes")
+
+    # Append the different data types
+    DuckDB.duckdb_append_bool(appender, true)
+    DuckDB.duckdb_append_int8(appender, 1)
+    DuckDB.duckdb_append_int16(appender, 2)
+    DuckDB.duckdb_append_int32(appender, 3)
+    DuckDB.duckdb_append_int64(appender, 4)
+    DuckDB.duckdb_append_uint8(appender, 1)
+    DuckDB.duckdb_append_uint16(appender, 2)
+    DuckDB.duckdb_append_uint32(appender, 3)
+    DuckDB.duckdb_append_uint64(appender, 4)
+    DuckDB.duckdb_append_float(appender, 1.0)
+    DuckDB.duckdb_append_double(appender, 2.0)
+    DuckDB.duckdb_append_date(appender, 100)
+    DuckDB.duckdb_append_time(appender, 200)
+    DuckDB.duckdb_append_varchar(appender, "Foo")
+    # End the row of the appender
+    DuckDB.duckdb_appender_end_row(appender)
+    # Destroy the appender and flush the data
+    DuckDB.duckdb_appender_destroy(appender)
+
+    # Retrive the data from the table and store it in  a vector
+    df = DuckDB.toDataFrame(con, "select * from dtypes;")
+    data = Matrix(df)
+
+    # Test if the correct types have been appended to the table
+    @test data[1] === true
+    @test data[2] === Int8(1)
+    @test data[3] === Int16(2)
+    @test data[4] === Int32(3)
+    @test data[5] === Int64(4)
+    @test data[6] === UInt8(1)
+    @test data[7] === UInt16(2)
+    @test data[8] === UInt32(3)
+    @test data[9] === UInt64(4)
+    @test data[10] === Float32(1.0)
+    @test data[11] === Float64(2.0)
+    @test data[12] === Dates.Date("1970-04-11")
+    @test data[13] === Dates.Time(0, 0, 0, 0, 200)
+    @test data[14] === "Foo"
+
+    # Disconnect and close the database
     DuckDB.disconnect(con)
     DuckDB.close(db)
 end
